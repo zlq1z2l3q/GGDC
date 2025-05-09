8ad768680df30f18fe3f3bc095a4a284454daba9
zlq1z2l3q <zlq1z2l3q@hotmail.com>
2025-05-09 23:03:39 +0800

    Update preprocess_itw_im.py

--- util/preprocess_itw_im.py
+++ util/preprocess_itw_im.py
@@ -132,7 +132,7 @@ class preprocessInTheWildImage:
         shrink = int(np.floor(qsize / self.out_size * 0.5))
         if shrink > 1:
             rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
-            img = img.resize(rsize, Image.LANCZOS)
+            img = img.resize(rsize, Image.ANTIALIAS)
             quad /= shrink
             qsize /= shrink
 
@@ -167,7 +167,7 @@ class preprocessInTheWildImage:
         # Transform.
         img = img.transform((transform_size, transform_size), Image.QUAD, (quad + 0.5).flatten(), Image.BILINEAR)
         if self.out_size < transform_size:
-            img = img.resize((self.out_size, self.out_size), Image.LANCZOS)
+            img = img.resize((self.out_size, self.out_size), Image.ANTIALIAS)
 
         return img
 
